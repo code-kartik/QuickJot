30a9b4c4397306d8a7a78739b2862cc5d0ea770d
code-kartik <kartikarora23082003@gmail.com>
2023-06-12 17:53:19 +0530

    bugs added

--- app/src/main/java/com/example/quickjot/activities/MainActivity.kt
+++ app/src/main/java/com/example/quickjot/activities/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.quickjot.activities
 
 import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.provider.ContactsContract.CommonDataKinds.Note
+import android.util.Log
+import androidx.lifecycle.LiveData
+import com.example.quickjot.database.notesDatabase
 import com.example.quickjot.databinding.ActivityMainBinding
+import com.example.quickjot.entities.notes
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +26,20 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, CreateNoteActivity::class.java)
             startActivityForResult(intent, REQUEST_CODE)
         }
+        getNotes()
+    }
+
+    fun getNotes(){
+        class getNotesTask : AsyncTask<Void, Void, List<ContactsContract.CommonDataKinds.Note>>(){
+            override fun doInBackground(vararg p0: Void?): LiveData<List<notes>> {
+                return notesDatabase.getDatabase(applicationContext).notesDao.getAllNotes()
+            }
+
+            override fun onPostExecute(notes:List<Note>) {
+                super.onPostExecute(notes)
+                Log.d(notes.toString())
+            }
+        }
+        getNotesTask().execute()
     }
 }
