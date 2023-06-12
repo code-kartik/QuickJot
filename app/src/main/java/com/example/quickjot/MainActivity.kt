1acadd044d920cd224df3a307587775d3b2a4a8c
code-kartik <kartikarora23082003@gmail.com>
2023-06-12 11:12:19 +0530

    activity change on click

--- app/src/main/java/com/example/quickjot/MainActivity.kt
+++ app/src/main/java/com/example/quickjot/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.quickjot
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.quickjot.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    val REQUEST_CODE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.addNewNote.setOnClickListener {
+            val intent = Intent(this, CreateNoteActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE)
+        }
     }
 }
